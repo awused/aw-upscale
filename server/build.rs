36261e4df0ab990fc53e4fe1555c292ad64cf4a9
Desuwa <desuwa@desuwa.net>
2022-04-20 22:38:53 -0700

    Update dependencies, explicitly enable proto3 optional for now.

--- server/build.rs
+++ server/build.rs
@@ -1,3 +1,7 @@
 fn main() {
-    tonic_build::compile_protos("proto/upscale.proto").unwrap();
+    let mut config = prost_build::Config::new();
+    config.protoc_arg("--experimental_allow_proto3_optional");
+    tonic_build::configure()
+        .compile_with_config(config, &["proto/upscale.proto"], &["proto"])
+        .unwrap()
 }
